4aa428411852ef765f2f32988bb85b75cef082a1
unknown <cogjava3298@iiht.tech>
2022-11-17 17:14:53 +0530

    [#003] Created Member Controller template

---
+++ MemberRegistration-MemberService/MemberRegistration-MemberService/src/main/java/com/mrp/controller/MemberRegistrationController.java
@@ -0,0 +1,17 @@
+package com.mrp.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mrp.model.Member;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+@RequestMapping("/registration")
+public class MemberRegistrationController {
+
+}
